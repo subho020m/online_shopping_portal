188764ed9e440c8e636cb2adf1ce842d9b89f357
Subhajit Mukherjee <subhajitm6@gmail.com>
2014-07-03 14:02:47 +0530

    Online Shopping Portal

---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Online Coupon</title>
+	<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />	
+	<script type="text/javascript" src="js/java.js"></script>
+	
+</head>
+
+<body>
+<div id="outer">
+	<div id="wrapper">
+		<div id="nav">
+		  <div id="nav-left">
+		    <div id="nav-right">
+    			<ul>
+    			  <li><a href="index.html">HOME</a></li>
+    			  <li><a href="category.php">CATEGORY</a></li>
+    			  <li><a href="new.php">NEW COUPONS</a></li>
+    			  <li><a href="faq.php">FAQs</a></li>
+    			  <li><a href="contact.php">CONTACT</a></li>
+    			  <li><a href="register.php">REGISTER</a></li>
+    			</ul>
+		    </div>
+		  </div>
+			<div class="clear"></div>
+		</div>
+		<div id="head">
+			<div id="head-left"></div>
+			<div id="head-right"></div>
+
+			<h1><span class="logo"><span class="top">online-coupon</span><span class="gadgets">management-system</span></span></h1>
+			<div id="navb">
+			  <ul>
+				  <li><a href="user.php">USER</a></li>
+				  <li><a href="company.php">COMPANY</a></li>
+				</ul>
+			</div>
+		</div>
+
+  <div class="body">
+ 
+  
+</body>
+</html>
